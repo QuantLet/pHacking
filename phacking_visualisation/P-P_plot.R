431c0fd36b63e48905a3cadc7811035e629937be
iliasaga64 <iliasaga@gmail.com>
2021-02-24 17:01:45 +0100

    Updated P-P and Q-Q plots and code

--- phacking_visualisation/P-P_plot.R
+++ phacking_visualisation/P-P_plot.R
@@ -1,20 +1,31 @@
-n 		= 3
+skewed_uniform = function(n=1000, c=0.25)
+{
+  x1 = (runif(n)-1)/2
+  x2 = runif(n)/2
+  xx = runif(n)
+  xx = xx<c
+  x  = xx*x1+(1-xx)*x2
+  x  = sort(x)
+  return(x) 
+}
+n 		= 10
+c     = 0.25
 J 		= 1000
 t 		= c( rep(0, J) )
 nor 	= c( rep(0, J) )
 
 for (j in 1:J){
-  x      = runif(n)
-  t[j]   = sqrt(n) * ( (mean(x) - 0.5)/sd(x) )
+  x      = skewed_uniform(n,c)
+  t[j]   = sqrt(n) * ( (mean(x) - (-0.25*c+0.25*(1-c)))/sd(x) )
   nor[j] = pnorm(t[j])
 }
-t     = sort(t)
-t_q   = seq(0,J-1)/J
-nor_q = sort(nor)
-t_q_l=t_q[nor_q>0.99]
-nor_q_l=nor_q[nor_q>0.99]
-t_q_c=t_q[t_q>0.99]
-nor_q_c=nor_q[t_q>0.99]
+t       = sort(t)
+t_q     = seq(0,J-1)/J
+nor_q   = sort(nor)
+t_q_l   =t_q[nor_q>0.99]
+nor_q_l = nor_q[nor_q>0.99]
+t_q_c   = t_q[t_q>0.99]
+nor_q_c = nor_q[t_q>0.99]
 plot(t_q, nor_q, pch=16, col="steelblue")
 points(t_q_l, nor_q_l, pch=16, col="tomato")
 points(t_q_c, nor_q_c, pch=16, col="lightgreen")
--- phacking_visualisation/Q-Q_plot.R
+++ phacking_visualisation/Q-Q_plot.R
@@ -1,23 +1,35 @@
+skewed_uniform = function(n=1000, c=0.25)
+{
+  x1 = (runif(n)-1)/2
+  x2 = runif(n)/2
+  xx = runif(n)
+  xx = xx<c
+  x  = xx*x1+(1-xx)*x2
+  x  = sort(x)
+  return(x) 
+}
+
 n 		= 10
+c     = 0.75
 J 		= 10000
 t 		= c( rep(0, J) )
 
 for (j in 1:J){
-    x    = runif(n)
-    t[j] = sqrt(n) * ( (mean(x) - 0.5)/sd(x) )
+  x    = skewed_uniform(n,c)
+  t[j] = sqrt(n) * ( (mean(x) - (-0.25*c+0.25*(1-c)))/sd(x) )
 }
 t=sort(t)
 p_t 	= c( rep(0, 999) )
 p_n 	= c( rep(0, 999) )
 
 for (i in 1:999){
-    p_t[i] = t[i*10]
-    p_n[i] = qnorm(i/1000)
+  p_t[i] = t[i*10]
+  p_n[i] = qnorm(i/1000)
 }
-p_t_l=p_t[p_n< -2.326]
-p_n_l=p_n[p_n< -2.326]
-p_t_c=p_t[p_t< -2.326]
-p_n_c=p_n[p_t< -2.326]
+p_t_l = p_t[p_n< -2.326]
+p_n_l = p_n[p_n< -2.326]
+p_t_c = p_t[p_t< -2.326]
+p_n_c = p_n[p_t< -2.326]
 plot(p_t, p_n, pch=16, col="steelblue", xlim=c(-4,4), ylim=c(-4,4), xlab="",ylab="")
 points(p_t_c, p_n_c, pch=16, col="tomato")
 points(p_t_l, p_n_l, pch=16, col="lightgreen")
